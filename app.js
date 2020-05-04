6a36d4b83ad218dca12fec31e8d398c0845bb05e
nwankwoHENRY <19147572+nameisIRIS@users.noreply.github.com>
2020-05-04 17:04:24 +0100

    app.js updates

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
+const authRoutes = require('./routes/auth');
 const port = process.env.PORT || 3000;
 
-
+//Middlewares
 app.use(express.json());
+app.use(authRoutes);
+
+
 
 app.get('/', (req, res)=>{   res.send('They app is running'); });
 
 
 
-app.listen(port, `app running on http://localhost:${port}`);
+app.listen(port, ()=>console.log(`app running on http://localhost:${port}`));
