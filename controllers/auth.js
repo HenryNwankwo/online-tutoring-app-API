d86cc85a29181295dbe43d6d7c9c5b16b40c8d06
nwankwoHENRY <19147572+nameisIRIS@users.noreply.github.com>
2020-05-08 13:11:47 +0100

    adding get category feature

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,20 @@
 const Category = require('../models/categories');
 const User = require('../models/users');
 
-
+//Getting all categories
 exports.getCategory = (req, res, next)=>{
-
-    const name = req.body.name;   
-
+    
+    Category.find()
+        .then(result => {
+            res.status(200).send(result);
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({ "error": error });
+        });
 
 };
+
+//adding a new category
 exports.postCategory = (req, res, next) => {
 
     const category = new Category({
@@ -24,15 +31,16 @@ exports.postCategory = (req, res, next) => {
     });
 
 };
+
+//adding a new subject in a category
 exports.createSubject = (req, res, next) => {
 
     const id = req.params.categoryId;
     const subjectName = req.body.name;
     Category.findByIdAndUpdate({_id:id}, {$push: {subjects: {name:subjectName}}})
     .then(result=>{
-        res.status(200).json({"message":"The subject has been added"});
-        console.log(result);
-        
+        res.status(200).json({"message":"The subject has been added"}).send(result.subjects);
+                
     }).catch(err => {
         console.log(err);
         res.status(500).json({"error" : error});
